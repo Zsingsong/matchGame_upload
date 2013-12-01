5971cb62856cfd48753f20bfb651c634997914f9
singsong <491273636@qq.con>
2013-12-01 11:34:16 +0800

    7

---
+++ matchGame/matchGame/RankViewController.h
@@ -0,0 +1,13 @@
+//
+//  RankViewController.h
+//  matchGame
+//
+//  Created by singsong on 13-12-1.
+//  Copyright (c) 2013年 singsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RankViewController : UIViewController
+
+@end
---
+++ matchGame/matchGame/RankViewController.m
@@ -0,0 +1,38 @@
+//
+//  RankViewController.m
+//  matchGame
+//
+//  Created by singsong on 13-12-1.
+//  Copyright (c) 2013年 singsong. All rights reserved.
+//
+
+#import "RankViewController.h"
+
+@interface RankViewController ()
+
+@end
+
+@implementation RankViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
